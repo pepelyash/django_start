4da8c97d6a21bacf6f675ba46e1e548ce8059bd3
pepelyash <spectraldemon@gmail.com>
2022-08-05 19:04:52 +0300

    2

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+# Create your views here.
 
 def hello_there(request, name):
     return render(
@@ -16,11 +19,28 @@ def hello_there(request, name):
         }
     )
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def about(request):
     return render(request, "hello/about.html")
 
 def contact(request):
     return render(request, "hello/contact.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
